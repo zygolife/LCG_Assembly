61a8f728bcdf7c6ba6df1141a429d557bac68c7c
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-22 18:54:57 -0700

    support new mode where there is more than on library per strain

--- Assembly/pipeline/00_make_fastq.sh
+++ Assembly/pipeline/00_make_fastq.sh
@@ -17,8 +17,12 @@ module load BBMap
 OUTDIR=input
 #FILE=$(ls $DATA/*.fastq.gz | sed -n ${N}p)
 BASE=$(sed -n ${N}p samples.dat | cut -f1)
-FILE=$(ls data/1978_Plate*/$BASE.fastq.gz)
+FILE=($(ls data/1978_*/$BASE.fastq.gz data/1978_*/$BASE.*.fastq.gz 2> /dev/null ))
 
-if [ ! -e $OUTDIR/${BASE}_R1.fq.gz ]; then
-	reformat.sh in=$FILE out1=$OUTDIR/${BASE}_R1.fq.gz out2=$OUTDIR/${BASE}_R2.fq.gz
+if [[ ! -f $OUTDIR/${BASE}_R1.fq.gz && ! -s $OUTDIR/${BASE}_R2.fq.gz ]]; then
+	rm -f $OUTDIR/${BASE}_R?.fq.gz
+	for file in "${FILE[@]}"
+	do
+		reformat.sh in=$file out1=$OUTDIR/${BASE}_R1.fq.gz out2=$OUTDIR/${BASE}_R2.fq.gz app=true
+	done
 fi
