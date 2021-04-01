3db68202e53591f2edfacc1276090788e06bfc84
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-31 22:33:31 -0700

    report buscos that need to be rerun

--- Assembly/scripts/find_missing_busco.sh
+++ Assembly/scripts/find_missing_busco.sh
@@ -1,11 +1,11 @@
 n=1
 cut -f1 samples.dat | while read strain; do 
-	if [ ! -d BUSCO/run_${strain} ]; then echo "$n $strain"; fi; 
+	if [ ! -d BUSCO/${strain} ]; then echo "$n $strain"; fi; 
 	n=$(expr $n + 1);
 done
 n=1
 m=$(cut -f1 samples.dat | while read strain; do 
-if [ ! -d BUSCO/run_${strain} ]; then echo "$n"; fi; 
+if [ ! -d BUSCO/${strain} ]; then echo "$n"; fi; 
 n=$(expr $n + 1); 
 done | perl -p -e 's/\n/,/' | perl -p -e 's/,$//')
 echo "sbatch --array=$m pipeline/03_BUSCO.sh"
