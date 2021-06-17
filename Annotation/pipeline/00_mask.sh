54b696dc8e6802521c46c1899db2520a54d09b66
Jason Stajich <jasonstajich.phd@gmail.com>
2021-06-17 15:16:19 -0700

    run with full path and using new scratch - rework to use samples_prefix as input

--- Annotation/pipeline/00_mask.sh
+++ Annotation/pipeline/00_mask.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH -p batch --time 2-0:00:00 --ntasks 8 --nodes 1 --mem 24G --out logs/mask.%a.%A.log
-
+module load workspace/scratch
 CPU=1
 if [ $SLURM_CPUS_ON_NODE ]; then
     CPU=$SLURM_CPUS_ON_NODE
 fi
 
-INDIR=genomes
-OUTDIR=genomes
-LIBRARY=lib/zygo_repeats.fasta
-SAMPFILE=samples.csv
+INDIR=$(realpath genomes)
+OUTDIR=$(realpath genomes)
+LIBRARY=$(realpath lib/zygo_repeats.fasta)
+
+SAMPFILE=samples_prefix.csv
 N=${SLURM_ARRAY_TASK_ID}
 
-if [ ! $N ]; then
+if [ -z $N ]; then
     N=$1
-    if [ ! $N ]; then
+    if [ -z $N ]; then
         echo "need to provide a number by --array or cmdline"
         exit
     fi
@@ -27,9 +28,12 @@ if [ $N -gt $MAX ]; then
 fi
 
 IFS=,
-cat $SAMPFILE | sed -n ${N}p | while read ProjID JGISample JGIProjName JGIBarcode SubPhyla Species Strain Note
+#SPECIES,STRAIN,JGILIBRARY,BIOSAMPLE,BIOPROJECT,TAXONOMY_ID,ORGANISM_NAME,SRA_SAMPID,SRA_RUNID,LOCUSTAG,TEMPLATE
+#ProjID JGISample JGIProjName JGIBarcode SubPhyla Species Strain Note
+
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read SPECIES STRAIN JGILIBRARY BIOSAMPLE BIOPROJECT TAXONOMY_ID ORGANISM_NAME SRA_SAMPID SRA_RUNID LOCUSTAG TEMPLATE
 do
- name=$(echo "$Species" | perl -p -e 'chomp; s/\s+/_/g')
+ name=$(echo "$SPECIES" | perl -p -e 'chomp; s/\s+/_/g')
  fixedname=$(echo $name | perl -p -e 's/\+/Plus/')
 
  if [[ $name != $fixedname ]]; then
@@ -41,25 +45,22 @@ do
      exit
  fi
 
-if [ ! -f $OUTDIR/${name}.masked.fasta ]; then
-module load funannotate
-
-export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
-
+ if [ ! -f $OUTDIR/${name}.masked.fasta ]; then
+     module load funannotate
+     export AUGUSTUS_CONFIG_PATH=$(realpath lib/augustus/3.3/config)
    # export AUGUSTUS_CONFIG_PATH=/bigdata/stajichlab/shared/pkg/augustus/3.3/config
-
-    #funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta
-if [ -f repeat_library/${name}.repeatmodeler-library.fasta ]; then
-	    LIBRARY=repeat_library/${name}.repeatmodeler-library.fasta
-fi
-    LIBRARY=$(realpath $LIBRARY)
-    mkdir $name.mask.$$
-    pushd $name.mask.$$
-    funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta -l $LIBRARY -m repeatmasker
-    mv funannotate-mask.log ../logs/${name}.mask.log
-    popd
-    rmdir $name.mask.$$
-else 
-    echo "Skipping ${name} as masked already"
-fi
+     
+     #funannotate mask --cpus $CPU -i ../$INDIR/${name}.sorted.fasta -o ../$OUTDIR/${name}.masked.fasta
+     if [ -f repeat_library/${name}.repeatmodeler-library.fasta ]; then
+	 LIBRARY=repeat_library/${name}.repeatmodeler-library.fasta
+     fi
+     LIBRARY=$(realpath $LIBRARY)
+     cwd=`pwd`
+     pushd $SCRATCH
+     funannotate mask --cpus $CPU -i $INDIR/${name}.sorted.fasta -o $OUTDIR/${name}.masked.fasta -l $LIBRARY -m repeatmasker
+     mv funannotate-mask.log $cwd/logs/${name}.mask.log
+     popd
+ else 
+     echo "Skipping ${name} as masked already"
+ fi
 done
